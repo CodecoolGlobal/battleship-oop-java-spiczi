93642dd2d708c615a3d9c853f888ec1a6b7b3e6b
Mate Papai <papaimate166@gmail.com>
2021-06-09 14:32:25 +0200

    Update Ship.java

--- BattleShip/src/Ship.java
+++ BattleShip/src/Ship.java
@@ -1,43 +1,48 @@
 import java.util.List;
 
 public class Ship {
+
     private List<Square> NewShip;
     private ShipType shipType;
 
-    public Ship(List<Square> NewShip, ShipType shipType){
-        NewShip = NewShip;
+    public Ship(List<Square> newShip, ShipType shipType) {
+        NewShip = newShip;
         this.shipType = shipType;
     }
-    public  List<Square> getNewShip(){
+
+    public List<Square> getFields() {
         return NewShip;
     }
-    public ShipType getShipType(){
+
+    public ShipType shipType() {
         return shipType;
     }
-    public void add(Square square){
-        NewShip.add(square);
 
-    //vallidaci a hajok elhelyezesere! A count
-    public boolean PlacementCheck(Ship ship1, List<Ship> Ships, Board board) {
+    public void add(Square square) {
+        NewShip.add(square);
+    }
+
+    public boolean isPlacementOK(Ship ship1, List<Ship> ships, Board board) {
         int count = 0;
-        for (int i = 0; i < ship1.getNewShip().size(); i++) {//vegig megyünk a mereten az 1 hajonak
-            if (ship1.getNewShip().get(i).getY() > board.getSizeY() ||//hajo i edik elememenk y kordinataja nagyobb
-                    ship1.getNewShip().get(i).getX() > board.getSizeX()) {//mint a tabla Y kordinata maxa vagy hajo i
-                count++;//edik elemenek x kordinataja nagyobb mint tabla max xe akkr növeleje a countott 1!
+        for (int i = 0; i < ship1.getFields().size(); i++) {
+            if(ship1.getFields().get(i).getY()> board.getSizeY() ||
+                    ship1.getFields().get(i).getX()> board.getSizeX()) {
+                count++;
+
             }
-            for (int j = 0; j < Ships.size(); j++) { //vegig megyüpnk a hajo mereten ismet 2 x mert x es y fekvesü hajonk
-                for (int k = 0; k < Ships.size(); k++) {//is lehet es hajo i edik elemenek x kordinataja es a y koardinataja
-                    if (ship1.getNewShip().get(i).getX() == Ships.get(k).getNewShip().get(k).getX() &&
-                            ship1.getNewShip().get(i).getY() == Ships.get(k).getNewShip().get(k).getY()) {
-                        count++;// egyenlö a k adik ellem x es y kordinatajaval akkor ismet növelje a countot 1-el
-                    }
+            for(int k = 0; k < ships.size(); k++) {
+                for(int z = 0; z < ships.get(k).getFields().size(); z++) {
+                  if(ship1.getFields().get(i).getX() == ships.get(k).getFields().get(z).getX() &&
+                          ship1.getFields().get(i).getY() == ships.get(k).getFields().get(z).getY()) {
+                      count++;
+                  }
                 }
             }
-        }if (count==0){
+        }
+        if(count == 0) {
             return true;
         }
         return false;
     }
-
 }
 
